0bea45a24ca4424c4b2b1944f452ca2f70fc4dbd
sunnypeeh4real <okaforsundaynn@gmail.com>
2022-11-30 03:25:07 +0000

    my advanced task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-for ascii_num in range(122, 96, -1):
-	if ascii_num % 2 == 1:
-		ascii_num = ascii_num - 32
-	print("{:c}".format(ascii_num), end='')
+i = 122
+while i >= 97:
+    flag = 0
+    if i % 2 != 0:
+        i = i - 32
+        flag = 1
+    print("{:s}".format(chr(i)), end="")
+    if flag == 1:
+        i = i + 32
+    i = i - 1
