be320a98d237cb3ce53e23eba013e7cff8a89751
sunnypeeh4real <okaforsundaynn@gmail.com>
2022-12-20 20:26:44 +0000

    Executable file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Working with OOP in python"""
+
+
+class Square():
+    """Implementing the square class"""
+    def __init__(self, size=0):
+        """ initialize the class
+        Args:
+           size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0 ")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Compute the area of the current square
+        Args:
+            None """
+        return (self.size * 4)
