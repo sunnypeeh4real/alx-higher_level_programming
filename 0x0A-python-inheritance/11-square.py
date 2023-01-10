bbe76d659d5ffea853e5b149545a22543979b4a5
sunnypeeh4real <okaforsundaynn@gmail.com>
2023-01-10 04:46:23 +0000

    my Task

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
-more class base
-"""
-
-
-Rectangle = __import__('9-rectangle').Rectangle
-
-
-"""
-Square class
+Created on Fri May 29 15:13:37 2020
+@author: Robinson Montes
 """
 
 
-class Square(Rectangle):
-    """ Square Class """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """
+    A Square class shape, inheirts from BaseGeometry
+    """
     def __init__(self, size):
-        """ size init"""
+        """"
+        Init function for Square
+        Attributes:
+            size (int): The size of the square
+        """
+        self.integer_validator("size", size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
+        """
+        str funtion to print with/height
+        Returns:
+            Return width/height
+        """
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+
+    def area(self):
+        """
+        A function that calculates the area of the Square
+        """
+        return self.__size ** 2
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-''' module: 4-inherits_from
-'''
+"""
+module only sub class of
+"""
 
 
 def inherits_from(obj, a_class):
-    '''the object is an instance of a class that inherited (directly or indirectly)
-    obj: an object
-    a_class: a class
-    returns None
-    '''
-    return type(obj) != a_class and isinstance(obj, a_class)
+    """returns True if the object is an instance of a class
+       that inherited (directly or indirectly) from the specified class;
+       otherwise False.
+    """
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
