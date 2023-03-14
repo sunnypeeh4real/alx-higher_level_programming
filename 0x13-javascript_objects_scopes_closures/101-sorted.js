b78cc668d4f6f2373f5fe0d925d9a8105815f98e
sunnypeeh4real <okaforsundaynn@gmail.com>
2023-03-14 21:08:30 +0000

    Executable File

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-exports.dict = {
-    89: 1,
-    90: 2,
-    91: 1,
-    92: 3,
-    93: 1,
-    94: 2
-};
+// Script imports a dictionary of occurrences by user id
+// and computes a dictionary of user ids by occurrence.
+
+const { dict } = require('./101-data.js');
+const Dictn = {};
+for (const N in dict) {
+    if (Dictn[dict[N]] === undefined) {
+	Dictn[dict[N]] = [];
+    }
+    Dictn[dict[N]].push(N);
+}
+console.log(Dictn);
